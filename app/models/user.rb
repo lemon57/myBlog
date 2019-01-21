c05e9581edd2f18cf6ea55f291ba8bd02c117d9a
Roman Lemon <gzeek57@gmail.com>
2019-01-21 23:29:57 +0100

    add signing up users, add bcrypt, and has_secure_password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
-VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  has_secure_password
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   before_save { self.email = email.downcase }
   has_many :articles
   validates :username, presence: true,
